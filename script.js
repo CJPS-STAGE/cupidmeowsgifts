b9000598753f7592cc9bd9d87aeebe485469c0da
CJPS STAGE <mitskinumber1stan@gmail.com>
2026-02-14 10:58:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function selectCategory(category) {
 const questions = [
   {
     question: "Love is in the air! You’re watching BakingWithJoni while sipping a cup of chrysanthemum tea. You take mental notes— knowing you won’t bake the fruit tarts anyway. It was truly a peaceful da- *BANG, BANG, BANG!* You open the door with a groan.",
-    image: "images/Q1.png",
+    image: "q1.gif",
     answers: {
       A: { text: "What genre of mormon is at my doorstep this time..", scores: { shin: 1, sof: 1 } },
       B: { text: "This is LITERALLY the start of every horror game.", scores: { chu: 1, ciel: 1, fel: 1, joni: 1, sham: 1 } },
@@ -26,7 +26,7 @@ const questions = [
   },
   {
     question: "At the doorstep, you see a bakery box with a tag attached. You examine it—‘Goods by BakingWithJoni’. It’s from the show you were watching..?",
-    image: "images/Q2.png",
+    image: "q2.png",
     answers: {
       A: { text: "It can’t be.. Is this.. the start of my K-Drama?!?", scores: { chu: 1, sof: 1, ciel: 1, fel: 1 } },
       B: { text: "OH MY GOD ITS A STALKER HORROR GAME", scores: { sham: 1, alb: 1, joni: 1, alex: 1 } },
@@ -36,7 +36,7 @@ const questions = [
   },
   {
     question: "Suddenly, a cat-like figure bursts out of the box. “Love looks not with the eyes, but with the mind; And therefore is winged Cupid painted blind. Tis I, Cupid Meow! I was called upon this mortal realm to fix your love life~” Cupid Meow hums.",
-    image: "images/Q1.png",
+    image: "q3.png",
     answers: {
       A: { text: "I’m calling the cops.", scores: { gion: 1, emi: 1, nor: 1, leon: 1, shin: 1, ric: 1 } },
       B: { text: "OH MY GOD PLEASE DON’T KILL ME I’M SORRY I IGNORED THE STARVING STRAY CAT AT THE STREET 2 DAYS AGO", scores: { fel: 1, joni: 1, sham: 1, alex: 1 } },
@@ -46,7 +46,7 @@ const questions = [
   },
   {
     question: "“Uh…. Anyways! This fruit tart represents balance in your relationship. The blackberries represent the real love that exists when you know someone completely. The strawberries represent the happiness and purity in love. The raspberries represent the kindness and virtue needed in love. To have the greatest love, you must eat this fruit tart with the right amount of intention!” Cupid Meow finishes.",
-    image: "images/Q1.png",
+    image: "q4.gif",
     answers: {
       A: { text: "Not reading allat. *eats the fruit tart*", scores: { chu: 1, anu: 1, ric: 1 } },
       B: { text: "Uhh.. *picks out the fruits you dislike and eats it* That’s better!", scores: { emi: 1, mia: 1, kim: 1, leon: 1, alb: 1, ciel: 1 } },
@@ -56,7 +56,7 @@ const questions = [
   },
   {
     question: "“Wait— Hold on! You’re not doing it prope—“ Cupid Meow gets interrupted by the loud groan coming from the bakery box. The bakery box grew bigger and bigger, caging both of you in the process.",
-    image: "images/Q1.png",
+    image: "q5.gif",
     answers: {
       A: { text: "..Why didn’t I call the cops?", scores: { alb: 1, gion: 1, nor: 1, ric: 1, sof: 1, char: 1, leon: 1 } },
       B: { text: "I’M GONNA DIE!!!!", scores: { mia: 1, alex: 1, sham: 1, emi: 1, chu: 1, shin: 1 } },
@@ -66,7 +66,7 @@ const questions = [
   },
   {
     question: "While you were caught off guard, the floor shifted into an endless series of spiral staircases. You tumble and tumble continuously until you feel Cupid Meow hook and slam you onto the ground. “Woah! Rough ride, huh? I apologise for that..” Cupid Meow says awkwardly. You ignore him and examine the place.",
-    image: "images/Q1.png",
+    image: "q6.gif",
     answers: {
       A: { text: "Is this a Roblox obby?", scores: { alb: 1, gion: 1, nor: 1, ric: 1, sof: 1, char: 1, leon: 1 } },
       B: { text: "*takes out phone to call the cops* Damn it! No signal…", scores: { mia: 1, alex: 1, sham: 1, emi: 1, chu: 1, shin: 1 } },
@@ -76,7 +76,7 @@ const questions = [
   },
   {
     question: "“No, you’ve got it all wrong! Y-you see… you’re not supposed to be here. You disrupted the balance placed into the fruit tart and now you’re here. I’ll help you out, swear!!!” Cupid Meow stutters. You sigh and look around for a way out. To your surprise, you spot figures with… valentine boxes as their heads.",
-    image: "images/Q1.png",
+    image: "q7.png",
     answers: {
       A: { text: "You approach them", scores: { alex: 1, alb: 1, leon: 1, emi: 1, cor: 1, ric: 1 } },
       B: { text: "You hide", scores: { kim: 1, mia: 1, liv: 1, sham: 1, anu: 1, char: 1 } },
@@ -86,7 +86,7 @@ const questions = [
   },
   {
     question: "The valentine box headed figures immediately noticed your presence. Before you could react, they rushed at you at a rapid speed. CLING CLANK CLING CLANK",
-    image: "images/Q1.png",
+    image: "q8.gif",
     answers: {
       A: { text: "You run away", scores: { chu: 1, fel: 1, gion: 1, liv: 1, joni: 1, nor: 1, sof: 1, char: 1 } },
       B: { text: "You sob and collapse onto the ground", scores: { mia: 1, sham: 1, chu: 1, ciel: 1 } },
@@ -96,7 +96,7 @@ const questions = [
   },
   {
     question: "“Halt, That peasants mine!” a commanding voice shouted. The valentine box headed figures immediately froze. Shortly, a brunette man in royal clothing approaches you. “I am Prince Gion of the Ice Kingdom. You are currently trespassing on our kingdom's borders, peasant outlander. Do you require assistance?”",
-    image: "images/Q1.png",
+    image: "q9.png",
     answers: {
       A: { text: "WHO are you calling a peasant?!", scores: { sof: 1, ciel: 1, alb: 1, joni: 1, chu: 1, shin: 1, anu: 1, emi: 1, ric: 1 } },
       B: { text: "I’M SORRY I’M SORRY I’M SORRY!!!", scores: { sham: 1, mia: 1, fel: 1, alex: 1 } },
@@ -106,7 +106,7 @@ const questions = [
   },
   {
     question: "“Hey, back off! They’re mine!” Cupid Meow yells as he shields you with his candy cane. “Hmph, you love fairies and your ridiculous magic. This peasant is better off with me.” Prince Gion argues. “They’re lost— I need to help them escape!” Cupid Meow pleas. “Hah, I'm assuming YOU were the one that brought the peasant here.” Prince Gion scoffs. “..Well, whatever! They would definitely trust their life with ME over YOU! …Right?” Cupid Meow asks you.",
-    image: "images/Q1.png",
+    image: "q10.gif",
     answers: {
       A: { text: "I’d trust my life with you, Cupid Meow!", scores: { joni: 1, cor: 1, alex: 1, alb: 1, ciel: 1, fel: 1, chu: 1 } },
       B: { text: "I’d rather trust my life with Prince Gion..", scores: { shin: 1, liv: 1, anu: 1, emi: 1, nor: 1, kim: 1, sof: 1 } },
@@ -115,7 +115,7 @@ const questions = [
   },
   {
     question: "“HUUUUUUH?!?!?” Prince Gion flashes an evil smirk. “Guards, seize this peasant now!” he immediately commands. You finally realise the situation you’re in. You ran away from the commotion, looking for a hiding spot. Suddenly, you stumble onto someone by accident. “AAAAH!” a soft voice yelps.",
-    image: "images/Q2.png",
+    image: "q11.png",
     answers: {
       A: { text: "OH GOD I’M SO SORRY PLEASE DON’T KILL ME", scores: { sham: 1, ciel: 1, fel: 1, chu: 1 } },
       B: { text: "I’m sorry! Are you alright?", scores: { liv: 1, alex: 1, mia: 1, joni: 1, leon: 1, alb: 1, ric: 1, emi: 1 } },
@@ -125,7 +125,7 @@ const questions = [
   },
   {
     question: "You look up to see two women who appear to be mages. “Oh dear, are you alright? Please, take this herb. It will heal any minor injuries.” a mother-like mage says. “I-i’m so sorry! I’ll make sure to be more careful!” a timid voice followed.",
-    image: "images/Q2.png",
+    image: "q12.png",
     answers: {
       A: { text: "A fruit tart sent me to a whole different dimension. What makes you think I’ll accept herbs from a mage?", scores: { shin: 1, anu: 1, sof: 1, emi: 1, leon: 1, gion: 1 } },
       B: { text: "Damn, I prefer women too.", scores: { joni: 1, ciel: 1, fel: 1, chu: 1 } },
@@ -135,14 +135,14 @@ const questions = [
   },
   {
     question: "“Don’t fret. I am Mage Neelam. The Wise and Generous of the Candy Borders.” Mage Neelam says softly. “Um.. I’m Mia, her apprentice.” Mia says with a crooked smile.",
-    image: "images/Q2.png",
+    image: "q13.png",
     answers: {
       A: { text: "Candy Borders..? Miss, I’m not from this dimension! Please take me back home!", scores: { shin: 1, anu: 1, sof: 1, emi: 1, leon: 1, gion: 1, joni: 1, ciel: 1, felix: 1, chu: 1, alex: 1, sham: 1, mia: 1, liv: 1, alb: 1, char: 1, nor: 1, cor: 1, ric: 1, kim: 1 } }
     }
   },
   {
     question: "“H-huh, really?! That explains the major changes in the borders earlier..” Mia responds. “Ah, yes. You are correct, Mia. This is quite the case. Well, our outlander shouldn’t overstay their welcome. I shall bring you back now.” Mage Neelam says as she waves her sceptre.",
-    image: "images/Q2.png",
+    image: "q14.png",
     answers: {
       A: { text: "Damn, finally! This advertisement is way too long.", scores: { kim: 1, nor: 1, ric: 1, anu: 1, shin: 1 } },
       B: { text: "I miss my home…", scores: { mia: 1, cor: 1, alex: 1, liv: 1, char: 1, gion: 1, leon: 1 } },
@@ -152,7 +152,7 @@ const questions = [
   },
   {
     question: "You open your eyes to BakingWithJoni still playing on the television. You frantically grab the remote and shut it down. Now, only one thought lingers in your mind.",
-    image: "images/Q2.png",
+    image: "q15.png",
     answers: {
       A: { text: "How exactly did this fix my love life?", scores: { shin: 1, anu: 1, sof: 1, emi: 1, leon: 1, gion: 1, joni: 1, ciel: 1, felix: 1, chu: 1, alex: 1, sham: 1, mia: 1, liv: 1, alb: 1, char: 1, nor: 1, cor: 1, ric: 1, kim: 1 } }
     }
@@ -370,3 +370,4 @@ function showResult() {
   document.getElementById('quiz').style.display = 'none';
   document.getElementById('result').style.display = 'block';
 }
+
